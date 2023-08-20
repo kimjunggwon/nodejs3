7637b3a290512372c99504b9727e3272c2765b9f
overroading <overroading@gmail.com>
2023-08-21 00:32:54 +0900

    230821

--- chapter10/nest-auth-test/src/main.ts
+++ chapter10/nest-auth-test/src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //전역 파이프에 validationPipe 객체 추가
-  app.userGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
